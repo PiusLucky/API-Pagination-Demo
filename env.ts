9ea9cb2441b5e6effc3a22652dc88627cb2bd004
PiusLucky <luckypius50@gmail.com>
2023-11-25 11:02:20 +0100

    chore: added proper environment setup

---
+++ env.ts
@@ -0,0 +1,13 @@
+import { cleanEnv, port, str } from "envalid";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const env = cleanEnv(process.env, {
+  PORT: port(),
+  DBPASSWORD: str(),
+  DATABASE_NAME: str(),
+  DATABASE_USERNAME: str(),
+});
+
+export default env;
--- src/providers/sequelize.ts
+++ src/providers/sequelize.ts
@@ -1,28 +1,34 @@
 import { Sequelize } from "sequelize";
+import env from "../../env";
 
 let sequelize: Sequelize;
 
 console.info(
-  "************ <<<< STARTING TESTING DATABASE >>>> *******************"
+  "************ <<<< STARTING DEVELOPMENT DATABASE >>>> *******************"
 );
 
-const DBPASSWORD = "YOUR_PASWORD";
+const { DATABASE_NAME, DATABASE_USERNAME, DBPASSWORD } = env;
 
-sequelize = new Sequelize("DATABASE_NAME", "postgres", DBPASSWORD, {
-  host: "localhost",
-  dialect: "postgres",
-  logging: false,
+sequelize = new Sequelize(
+  DATABASE_NAME as string,
+  DATABASE_USERNAME as string,
+  DBPASSWORD as string,
+  {
+    host: "localhost",
+    dialect: "postgres",
+    logging: false,
 
-  define: {
-    timestamps: true,
-  },
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+    define: {
+      timestamps: true,
+    },
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
 process.on("SIGINT", () => {
   sequelize.close().then(() => {
